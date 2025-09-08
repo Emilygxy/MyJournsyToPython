5e4679e228cf91f85ab25a8ebfa9e85cc377ef0e
emily <emily@gridworld.com>
2025-09-08 13:26:37 +0800

    add module based on learning plane

---
+++ Math_PY/MathLibries/BasicMath.py
@@ -0,0 +1,9 @@
+
+class MathUtils:
+    def matrix_multiply(self, A, B):
+        # 矩阵乘法实现
+        pass
+    
+    def gradient_descent(self, loss_func, params, lr=0.01):
+        # 梯度下降实现
+        pass
